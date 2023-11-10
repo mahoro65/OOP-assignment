7c86e1692866209ef7d32769a790424d9cca711c
mahoro65 <143531273+mahoro65@users.noreply.github.com>
2023-11-10 05:52:19 -0800

    Add files via upload

---
+++ OOP/intro.py
@@ -0,0 +1,18 @@
+#create a user class with properties ie name,age,location
+class adult:
+    def __init__(baby, name, age, location):
+        baby.name = name
+        baby.age = age
+        baby.location = location
+    def myfunction(adult):#function to return the location
+      print(p1.location)
+p1 = adult("mahoro", 50,"kampala")
+print(p1.name,p1.age)#adding to return name and age
+p1.myfunction()#function to print user location
+
+
+ 
+    
+
+
+
